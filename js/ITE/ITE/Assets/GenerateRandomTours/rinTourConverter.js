b794216ab755faf3d85a8a3c488368bd49b901c3
Karthik Harihar Reddy Battula <karthik_harihar_reddy_battula@brown.edu>
2014-07-28 18:28:32 +0530

    rin covertor changed

--- js/ITE/ITE/Assets/GenerateRandomTours/rinTourConverter.js
+++ js/ITE/ITE/Assets/GenerateRandomTours/rinTourConverter.js
@@ -21,23 +21,67 @@
 	                		for (var i in json.experiences[key].experienceStreams){
 	                			if (i === subprop){
 		                			newobj.name = key;
-		                			newobj.providerId = json.experiences[key].providerId;
+		                			if (json.experiences[key].providerId==="AES"){
+		                				newobj.providerId = "audio";	
+		                			}
+		                			else if (json.experiences[key].providerId==="ImageES"){
+		                				newobj.providerId = "image";	
+		                			}
+		                			else if (json.experiences[key].providerId==="ZMES"){
+		                				newobj.providerId = "deepZoom";	
+		                			}
+		                			else if (json.experiences[key].providerId==="VideoES"){
+		                				newobj.providerId = "video";	
+		                			}
+		                			else{
+		                				newobj.providerId = json.experiences[key].providerId;
+		                			}
 		                			//newobj.keyframes = json.experiences[key].experienceStreams[i].keyframes;
 	                                newobj.keyframes = [];
 	                                for (var k in json.experiences[key].experienceStreams[i].keyframes){
 	                                	var newkeyframe = {};
+	                                	var newkeyframe1 = {};
 	                                	// if (newobj.providerId===deepzoom){
 		                                // 	newkeyframe.time = k.offset;
 		                                // 	newkeyframe.pos = k.state.viewport.region.center;	
 	                                	// }
-	                                	
-	                                		newkeyframe.time = json.experiences[key].experienceStreams[i].keyframes[k].offset;
-		                                	if (typeof json.experiences[key].experienceStreams[i].keyframes[k].state.viewport !== "undefined"){
-			                                	newkeyframe.size = json.experiences[key].experienceStreams[i].keyframes[k].state.viewport.span;
-			                                	newkeyframe.pos = json.experiences[key].experienceStreams[i].keyframes[k].state.viewport.region.center;
-		                                }
+	                                	if (k===0){
+	                                		newkeyframe.time = 0;
+	                                		newkeyframe.opacity = 0;
+	                                		if (typeof json.experiences[key].experienceStreams[i].keyframes[0].state.viewport !== "undefined"){
+	                                			if(newobj.providerId="deepZoom"){
+	                            	    			newkeyframe.scale = 1; //for now
+		                                			newkeyframe.pos = json.experiences[key].experienceStreams[i].keyframes[0].state.viewport.region.center;
+		                                		}
+		                                		else if(newobj.providerId="audio"){
+		                                			if (typeof newkeyframe.volume = json.experiences[key].experienceStreams[i].keyframes[0].state.sound !== "undefined"){
+	                            	    				newkeyframe.volume = json.experiences[key].experienceStreams[i].keyframes[0].state.sound.volume;
+	                            	    			}
+	                            	    			else{
+	                            	    				newkeyframe.volume = 0.5;
+	                            	    			}	
+		                                			newkeyframe.audioOffset = 
+		                                		    
+		                                		}
+	                                    	}
+	                                    	newobj.keyframes.push(newkeyframe);
+	                                    	newkeyframe1.time = 1;
+
+
+	                                	}
+	                                	    
+	                                	newkeyframe.time = json.experiences[key].experienceStreams[i].keyframes[k].offset;
+	                                	if (typeof json.experiences[key].experienceStreams[i].keyframes[k].state.viewport !== "undefined"){
+	                            	    	newkeyframe.size = json.experiences[key].experienceStreams[i].keyframes[k].state.viewport.region.span;
+		                                	newkeyframe.pos = json.experiences[key].experienceStreams[i].keyframes[k].state.viewport.region.center;
+	                                    }
 		                                
-			                                newobj.keyframes.push(newkeyframe);
+		                                newobj.keyframes.push(newkeyframe);
+	                                }
+	                                for (var l in newobj.keyframes){
+	                                	if (l===1 || l===2){
+
+	                                	}
 	                                }
 	                			if (typeof json.experiences[key].resourceReferences[0] !== "undefined" ){
 	                				var resourceid = json.experiences[key].resourceReferences[0].resourceId;
