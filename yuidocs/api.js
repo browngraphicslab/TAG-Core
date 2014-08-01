74404ed1bb115334b327faefa19b613dbbdce6e4
Ben LeVeque <ben.leveque@gmail.com>
2014-08-01 15:13:47 -0400

    added yuidocs directory to index on this branch

---
+++ yuidocs/api.js
@@ -0,0 +1,39 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "TAG.AnnotatedImage",
+        "TAG.Authoring.EditorMenu",
+        "TAG.Layout.ArtworkEditor",
+        "TAG.Layout.ArtworkViewer",
+        "TAG.Layout.CollectionsPage",
+        "TAG.Layout.InternetFailurePage.js",
+        "TAG.Layout.StartPage",
+        "TAG.Layout.VideoPlayer",
+        "TAG.TourAuthoring.ArtworkTrack",
+        "TAG.TourAuthoring.AudioTrack",
+        "TAG.TourAuthoring.Command",
+        "TAG.TourAuthoring.ComponentControls",
+        "TAG.TourAuthoring.Display",
+        "TAG.TourAuthoring.ImageTrack",
+        "TAG.TourAuthoring.InkAuthoring",
+        "TAG.TourAuthoring.InkTrack",
+        "TAG.TourAuthoring.Keyframe",
+        "TAG.TourAuthoring.PlaybackControl",
+        "TAG.TourAuthoring.TimeManager",
+        "TAG.TourAuthoring.Timeline",
+        "TAG.TourAuthoring.TopMenu",
+        "TAG.TourAuthoring.TourOptions",
+        "TAG.TourAuthoring.Track",
+        "TAG.TourAuthoring.UndoManager",
+        "TAG.TourAuthoring.VideoTrack",
+        "TAG.TourAuthoring.Viewer",
+        "TAG.Util.Artwork",
+        "TAG.Util.IdleTimer",
+        "TAG.Util.Splitscreen",
+        "Telemetry",
+        "tagInk"
+    ],
+    "modules": [],
+    "allModules": []
+} };
+});
