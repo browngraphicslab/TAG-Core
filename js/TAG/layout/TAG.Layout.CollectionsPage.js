a9c2f1512673e0bac8a150ddd5d7fbc23cfec154
= <nhbowditch@gmail.com>
2015-06-01 16:32:10 -0400

    keywords on collections page!

--- js/TAG/layout/TAG.Layout.CollectionsPage.js
+++ js/TAG/layout/TAG.Layout.CollectionsPage.js
@@ -33,11 +33,12 @@ TAG.Layout.CollectionsPage = function (options) { // backInfo, backExhibition, c
         sortRow = root.find('#sortRow'),
         collectionMenu = root.find('#collectionMenu'),
         searchInput = root.find('#searchInput'),
+        keywordsDiv = root.find("#keywords"),
         searchTxt = root.find('#searchTxt'),
-        buttonRow = root.find('#buttonRow'),
+        // buttonRow = root.find('#buttonRow'), // replaced with #sorts (sortsDiv)
         artworksButton = root.find('#artworksButton'),
         assocMediaButton = root.find('#assocMediaButton'),
-        toggleRow = root.find('#toggleRow'),
+        // toggleRow = root.find('#toggleRow'), // replaced with #filters (filtersDiv)
         selectedArtworkContainer = root.find('#selectedArtworkContainer'),
         timelineArea = root.find('#timelineArea'),
         topBar = root.find('#topBar'),
@@ -45,6 +46,11 @@ TAG.Layout.CollectionsPage = function (options) { // backInfo, backExhibition, c
         infoButton = root.find('#infoButton'),
         tutorialButton = root.find('#tutorialButton'),
         linkButton = root.find('#linkButton'),
+        sortsDiv = root.find('#sorts'),
+        filtersDiv = root.find('#filters'),
+        divideDiv = root.find('#divide'),
+        keywordSelects = [], // Will be filled in later.
+        keywordOperatorSelects = [], // Will be filled in later.
         // splitscreenIcon          = root.find('#splitscreenIcon'),
         overlay = root.find('#overlay'),
         tileLoadingArea = root.find('#tileLoadingArea'),
@@ -318,6 +324,7 @@ TAG.Layout.CollectionsPage = function (options) { // backInfo, backExhibition, c
             'width'   : 'auto',
             'top'     : '22%',
         };
+
         /*
         var tileCircle = TAG.Util.showProgressCircle(tileDiv, progressCircCSS, '0px', '0px', false);
         tileLoadingArea.append(tileCircle);
@@ -653,6 +660,118 @@ TAG.Layout.CollectionsPage = function (options) { // backInfo, backExhibition, c
         }
         loadingArea.hide();
         searchInput.show();
+        addKeywords();
+    }
+
+    /**
+     * Adds keyword dropdowns to the search area.
+     * @method addKeywords
+     */
+    function addKeywords() {
+
+        // TODO: get actual keywords
+        var keywordCategories = ['Fruit, but this category title is going to be really long', 'Color', 'Genre'];
+        var keywords = [['Platonia', 'Bael', 'Cherymoya', 'Rambutan', 'Jabuticaba', 'Breadfruit', 'Noni'],
+                        ['Vermillion', 'Cerulean', 'Cinnibar', 'Viridian', 'Saffron', 'Fuschia'],
+                        ['Tropical House', 'Hardstyle', 'Crunkcore', 'Nerdcore', 'Tagcore', 
+                        'Ambient Post-Noise-Metalcoretronicastep', 'Classical', 'Metamodernism', 'Realism',
+                        'Escapism', 'Genre', 'Meso-American', 'Brutalism', 'Grilled Cheese', 'Chuckie Cheese',
+                        'Charles Darwinism', 'Socialism', 'Schism', 'Sshh', 'Shitake', 'OMNONNONOMONOM', '^_^']];
+
+        // Start off by creating basic 'select' inputs. We will use jQuery 
+        // library 'dropdownchecklist' to make them look nicer. 
+        if (keywordCategories.length > 0) {
+            // Create unordered list of dropdowns.
+            var ddList = $(document.createElement('ul')).addClass('rowLeft'); // class keeps stuff inline and hides bullets
+
+            // Loop through the categories of keywords. 
+            keywordCategories.forEach(function(element, index, array) {
+                // Each category has 2 dropdowns, one for the operator AND/NOT, and one for selecting multiple keywords.
+                // All dropdowns are added to the unordered list and kept inline.
+
+                // Create operator dropdown. 
+                var listItem1 = $(document.createElement('li')).addClass('rowItem'); // Class keeps list inline and spaces items.
+                var select1 = $(document.createElement('select')).addClass('keywordsSelect'); // Class stylizes dropdown.
+                var andOptn = $('<option>AND</option>').attr('value', '0'); // AND option.
+                var notOptn = $('<option>NOT</option>').attr('value', '1'); // NOT option.
+                select1.append(andOptn); // Add AND option.
+                select1.append(notOptn); // Add NOT option.
+                listItem1.append(select1); // Add actual dropdown to li.
+                ddList.append(listItem1); // Add the item to the list.
+                keywordOperatorSelects.push(select1); // Also add dropdown to stored list of operator dropdowns. 
+
+                // Create keywords dropdown.
+                var listItem2 = $(document.createElement('li')).addClass('rowItem'); // Class keeps list inline and spaces items.
+                var select2 = $(document.createElement('select')).addClass('keywordsSelect') // Class stylizes dropdown.
+                                                                 .addClass('multiselect') // Class to help distinguish multiselects. Might be unused.
+                                                                 .attr('multiple', 'multiple'); // Make this a multi-select element. jQuery lib will turn into dropdown.
+                for (var i = 0; i < keywords[index].length; i++) {
+                    // Add each of the keywords in this category.
+                    select2.append($('<option>' + keywords[index][i] + '</option>').attr('value', i.toString())); 
+                }
+                listItem2.append(select2); // Add actual dropdown to li.
+                ddList.append(listItem2); // Add the item to the list.
+                keywordSelects.push(select2); // Also add dropdown to stored list of keyword dropdowns.
+
+            }); 
+
+            // Finally, add the list of all the dropdowns to the keywords div.
+            keywordsDiv.append(ddList);
+
+            // Now run the dropdowncheclist jQuery library to turn the select elements into basic dropdown lists.
+            $('select.keywordsSelect').dropdownchecklist({maxDropHeight: $('#tagRootContainer').height() / 2, closeRadioOnClick: true});
+            
+            // Unfortunately, the dropdownchecklists are minimally stylized, so we need to do some cleaning up. 
+
+            // Format the dropdown selector box (what you click on to make dropdown appear).
+            $('.ui-dropdownchecklist-selector').each(function(index, element) {
+                // Set the text inside the selector box.
+                if (index % 2 == 0) {
+                    // Even numbered dropdowns are operator dropdowns.
+                    $(element).find('.ui-dropdownchecklist-text') // Get the text span element.
+                        .css('display', 'inline') // Make it inline so the div with our dropdown arrow will be inline.
+                        .css('color', '#000'); // Make the text black. TAG defaults spans to light gray.
+                    $(element).parent().parent().find('input').hide();//.css('opacity', '0'); // TODO: hiding the radio button creates bug where clicking item twice allows empty selection.
+                    //$(element).parent().parent().find('.ui-dropdownchecklist-text').css('margin-left', '15%'); // 
+
+                } else {
+                    // Odd numbered dropdowns are for keywords.
+                    var categoryIndex = (index - 1) / 2; // 1 --> 0, 3 --> 1, 5 --> 2, etc.
+                    $(element).text(function() { return keywordCategories[categoryIndex]; }); // Change the inner text of this selector element to category title.
+                    // Note: here we do not change the '.ui-dropdownchecklist-text' element (as we do above) to eliminate the functionality of updating
+                    // the selector with selected text. I.e., when a user selects a keyword, the selector box text will not change to that keyword, 
+                    // it will stay as the category title. 
+
+                    // Set the width of keywords dropdowns and make overflowing text have an ellipsis.
+                    $(element).css('width', $('#searchInput').width() + 'px')
+                              .css('overflow', 'hidden')
+                              .css('text-overflow', 'ellipsis');
+                }
+
+                // Further stylization of selector box.
+                var elementHeight = searchInput.innerHeight(); // Get the height of the search bar. We want the dropdowns to match it.
+                var selectorFontSize = 0.8 * elementHeight; // We want our font size to be slightly smaller than the box's height.
+                $(element).parent().css('height', elementHeight + 'px'); // This element uses padding, so we actually change the height of its parent, a wrapper span. 
+                $(element).css('line-height', selectorFontSize + 'px') // This centers the text in the selector box.
+                          .css('font-size', selectorFontSize + 'px') 
+                          .css('color', '#000'); // Make the text black.
+                          //.css('padding-right', elementHeight/2 + 'px'); // Add half the height to the width. This will leave room for a dropdown arrow.
+
+                // Create a dropdown arrow.
+                var arrowSize = elementHeight / 3; // Arrow is one third the box's height. Looks about right.
+                var downArrow = $(document.createElement('div')).css('float', 'right') // All done in css. Is only a div floating right in selector box.
+                            .css('width', '0')
+                            .css('height', '0')
+                            .css('margin-top', elementHeight/2 - arrowSize/2 + 'px') // This centers the arrow. Top margin is half the box's height minus half the arrow's height.
+                            .css('border-left', arrowSize + 'px solid transparent') 
+                            .css('border-right', arrowSize + 'px solid transparent') // Note to keep left and right borders transparent.
+                            .css('border-top',  arrowSize + 'px solid #000'); // Here we make the arrow black.
+                $(element).parent().append(downArrow); // Add the arrow the selector box.
+                $(element).parent().parent().find('.ui-dropdownchecklist-dropcontainer-wrapper') // Once the width of the selector box is set...
+                          .css('width', $(element).parent().outerWidth() + 'px'); // Change the width of the actual dropdownchecklist to be the same.
+
+            });
+        }
     }
 
     /**
@@ -731,7 +850,7 @@ TAG.Layout.CollectionsPage = function (options) { // backInfo, backExhibition, c
             if (onAssocMediaView && collection.collectionMediaMinYear===Infinity){
                 timelineShown = false;
             }
-            buttonRow.empty();
+            sortsDiv.empty();
 
             if (collection.Metadata.AssocMediaView && collection.Metadata.AssocMediaView === "true"){ 
                 TAG.Worktop.Database.getAssocMediaIn(collection.Identifier, function (mediaDoqs) {
@@ -1044,13 +1163,14 @@ TAG.Layout.CollectionsPage = function (options) { // backInfo, backExhibition, c
             catalogDiv.append(tileCircle);
 
             if (collection.Metadata.AssocMediaView && collection.Metadata.AssocMediaView === "true"){
-
-                toggleRow.css({
-                    'display': 'block',
-                });
-                if (TAG.Util.Splitscreen.isOn()) {
-                    toggleRow.css('width', '40%');
-                }
+                // toggleRow.css({
+                //     'display': 'block',
+                // });
+                // if (TAG.Util.Splitscreen.isOn()) {
+                //     toggleRow.css('width', '40%');
+                // }
+                filtersDiv.css('display','inline');
+                divideDiv.css('display','inline');
                 artworksButton.off()
                               .on('mousedown', function(){
                                     artworksButton.css('color', SECONDARY_FONT_COLOR);
@@ -1073,7 +1193,9 @@ TAG.Layout.CollectionsPage = function (options) { // backInfo, backExhibition, c
                                     }
                                 });
             } else {
-                toggleRow.css('display','none');
+                console.log(collection.Metadata);
+                filtersDiv.css('display','none');
+                divideDiv.css('display','none');
             }
            
             currCollection = collection;
@@ -1221,11 +1343,14 @@ TAG.Layout.CollectionsPage = function (options) { // backInfo, backExhibition, c
         function appendTags() {
             var i,
                 text;
-            buttonRow.empty();
+            //buttonRow.empty();
+            sortsDiv.empty();
             var uiDocfrag = document.createDocumentFragment();
+            var rowList = $(document.createElement('ul')).addClass('rowLeft');
             for (i = 0; i < sortOptions.length; i++) {
+                var listItem = $(document.createElement('li')).addClass('rowItem');
                 sortButton = $(document.createElement('div'));
-                uiDocfrag.appendChild(sortButton[0]);
+                // uiDocfrag.appendChild(sortButton[0]);
                 //Because stored on server as "Tour" but should be displayed as "Tours"
                 sortOptions[i]==="Tour" ? text = "Tours" : text = sortOptions[i];
                 sortButton.addClass('secondaryFont');
@@ -1248,6 +1373,8 @@ TAG.Layout.CollectionsPage = function (options) { // backInfo, backExhibition, c
                 //sortButton.css("height", newPos.height + 'px');
                 //buttonRow.append(sortButton);
                 sortButtonTags[sortButton.attr('id')] = sortOptions[i];
+                listItem.append(sortButton);
+                rowList.append(listItem);
 
                 //Sort telemetry register
                 TAG.Telemetry.register(sortButton, 'mousedown', 'SortOptions', function (tobj) {
@@ -1255,7 +1382,8 @@ TAG.Layout.CollectionsPage = function (options) { // backInfo, backExhibition, c
                     tobj.current_collection = currCollection.Identifier;
                 });
             }
-            buttonRow.append($(uiDocfrag));
+            uiDocfrag.appendChild(rowList[0]);
+            sortsDiv.append($(uiDocfrag));
             if (!comingBack || !currentTag) {
                 //If currentTag not defined currentTag is either 'year' or 'title' depending on if timeline is shown
                 if (timelineShown && (sortOptions.indexOf('Date')>=0)) {
@@ -1547,8 +1675,8 @@ TAG.Layout.CollectionsPage = function (options) { // backInfo, backExhibition, c
     function styleBottomContainer(){
         if (timelineShown){   
                 bottomContainer.css({
-                    'height' : '69%',
-                    'top' : '25%',
+                    'height' : '75%',
+                    'top' : '17%',
                     'z-index': '',
                 });
                 selectedArtworkContainer.css({
@@ -1558,7 +1686,7 @@ TAG.Layout.CollectionsPage = function (options) { // backInfo, backExhibition, c
         } else {
             bottomContainer.css({
                 'height':'79%',
-                'top':'15%',
+                'top':'13%',
                 'z-index':'100005',
             });
             selectedArtworkContainer.css({
