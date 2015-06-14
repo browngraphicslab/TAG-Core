f3847bda6b1a47ca660c2466c94c1e896ad282f3
Carlene Niguidula <carlene_niguidula@brown.edu>
2015-06-14 18:25:16 -0400

    fully disabled import tab when upload already happening

--- js/TAG/util/TAG.Util.js
+++ js/TAG/util/TAG.Util.js
@@ -4047,7 +4047,9 @@ TAG.Util.UI = (function () {
                 if (queueLength > 0 && tabs[i].name == 'Import') { //already upload happening - grey out and disable import tab only 
                     console.log("disable import tab");
                     tab.css({ 'opacity': '.4' });
-                    $(tab).prop('disabled', true); 
+                    tab.unbind('click');
+                } else{
+                    tab.on('click', tabHelper(i, tabs[i].name));
                 }
                 tab.css({
                     'display': 'inline-block',
@@ -4066,7 +4068,7 @@ TAG.Util.UI = (function () {
                     'font-size':'0.8em'
                 });
                 
-                tab.on('click', tabHelper(i, tabs[i].name));
+                
                 tabBanner.append(tab);
             }
             tab = $(document.createElement('div'));
