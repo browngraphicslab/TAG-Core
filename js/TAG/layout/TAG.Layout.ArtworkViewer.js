9e31ed961a7ddda715e6e01905e772a9f3ac248c
lucyvk <lucy_van_kleunen@brown.edu>
2015-06-22 19:21:39 -0400

    no splitscreen for locked artworks

--- js/TAG/layout/TAG.Layout.ArtworkViewer.js
+++ js/TAG/layout/TAG.Layout.ArtworkViewer.js
@@ -267,7 +267,7 @@ TAG.Layout.ArtworkViewer = function (options, container) { // prevInfo, options,
         root.append(splitscreenContainer);
         console.log('locked' + locked);
         console.log(!locked);
-        if (TAG.Util.Splitscreen.isOn() || locked==true) {
+        if (TAG.Util.Splitscreen.isOn() || locked) {
             splitscreenContainer.css('display', 'none');
         }
         else {
