955e51046c993dfe230403232a165576e1984c40
David Weinberger <lucy_van_kleunen@brown.edu>
2015-06-30 20:12:10 -0400

    re-adjusting vertical styling

--- js/TAG/layout/TAG.Layout.CollectionsPage.js
+++ js/TAG/layout/TAG.Layout.CollectionsPage.js
@@ -708,9 +708,11 @@ TAG.Layout.CollectionsPage = function (options) { // backInfo, backExhibition, c
         }
 
         //styling when keyword row is shown
+        /**
         if (showKeywords) {
             timelineArea.css({ "bottom": $("#tagRoot").height() * 0.020 + 'px' });
         }
+        **/
 
 
         // Start off by creating basic 'select' inputs. We will use jQuery library 'dropdownchecklist' to make them look nicer. 
